ec04cfb64147aad4e9a18d6a77ea0ddbc5301964
rasulovtim <timmprogramming@gmail.com>
2023-08-11 19:38:01 +0500

    New vers

--- calculator.java
+++ calculator.java
@@ -3,38 +3,44 @@ import java.util.*;
 public class calculator {
     static int digit1, digit2;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         System.out.println("Введите выражение(от 1 до 10). Например: 2 + 2 или IV / II");
         Scanner sc = new Scanner(System.in);
-        String [] input = (sc.nextLine()).split(" ");
-        if (input.length != 3){
+        String in = sc.nextLine();
+        System.out.println(calculate(in));}
+    public static String calculate(String inp)throws Exception{
+        String result;
+        String[] input = (inp.split(" "));
+        if (input.length != 3) {
             throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
         String operand1 = input[0];
         String operand2 = input[2];
         String operator = input[1];
-        if (romanToArab(operand1) != 0 && romanToArab (operand2) != 0){
+        if (romanToArab(operand1) != 0 && romanToArab(operand2) != 0) {
             int firstOperand = romanToArab(operand1);
             int secondOperand = romanToArab(operand2);
-            int resultForRomanInArab = (calc(firstOperand,secondOperand,operator)-1);
-            if (resultForRomanInArab < 0){
-                throw new Exception ("в римской системе нет 0 или отрицательных чисел");
-            }else{
-                System.out.println(convertArabToRoman(resultForRomanInArab));
+            int resultForRomanInArab = (calc(firstOperand, secondOperand, operator) - 1);
+            if (resultForRomanInArab < 0) {
+                throw new Exception("в римской системе нет 0 или отрицательных чисел");
+            } else {
+                result = String.valueOf(convertArabToRoman(resultForRomanInArab));
             }
-        }else{
+        } else {
             try {
                 digit1 = Integer.parseInt(operand1);
                 digit2 = Integer.parseInt(operand2);
-            } catch(NumberFormatException e) {
-                throw new NumberFormatException("т.к. используются одновременно разные системы счисления"+"\n"+e.getMessage());
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("т.к. используются одновременно разные системы счисления" + "\n" + e.getMessage());
             }
-            if ((digit1 >10 || digit1 <1) || (digit2 >10 || digit2 <1)){
+            if ((digit1 > 10 || digit1 < 1) || (digit2 > 10 || digit2 < 1)) {
                 throw new Exception("Введите число от 1 до 10");
             }
-            System.out.println(calc(digit1,digit2,operator));
+            result = String.valueOf((calc(digit1, digit2, operator)));
         }
+        return result;
     }
+
     public static int calc(int num1, int num2, String operation){
         int res = 0;
         switch (operation){
